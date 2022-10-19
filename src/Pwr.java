3cc29667bbcb308d7435abe42a5d5fbccb965c53
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-19 10:28:59 -0500

    init

--- src/Pwr.java
+++ src/Pwr.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Pwr {
+public class Pwr {
+
+  public static void main(String[] args) {
+    DemoPwr x = new DemoPwr(4.0, 2);
+    DemoPwr y = new DemoPwr(2.5, 1);
+    DemoPwr z = new DemoPwr(5.7, 0);
+
+    System.out.println(x.b + " raised to the " + x + " power is " + x.get_pwr());
+    System.out.println(y.b + " raised to the " + y + " power is " + y.get_pwr());
+    System.out.println(z.b + " raised to the " + z + " power is " + z.get_pwr());
+
+  }
+}
+
+class DemoPwr {
+  double b;
+  int e;
+  double val;
+
+  DemoPwr(double b, int e) {
+    this.b = b;
+    this.e = e;
+
+    val = 1;
+    if (e==0) return;
+    for (; e>0; e--) val = val * b;
+  }
+  double get_pwr() {
+    return val;
+  }
+
 }
