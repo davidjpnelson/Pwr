42a915ea4d227db39ddee3ecdc712e24bce4743e
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-19 11:05:29 -0500

    typo

--- src/Pwr.java
+++ src/Pwr.java
@@ -5,9 +5,9 @@ public class Pwr {
     DemoPwr y = new DemoPwr(2.5, 1);
     DemoPwr z = new DemoPwr(5.7, 0);
 
-    System.out.println(x.b + " raised to the " + x + " power is " + x.get_pwr());
-    System.out.println(y.b + " raised to the " + y + " power is " + y.get_pwr());
-    System.out.println(z.b + " raised to the " + z + " power is " + z.get_pwr());
+    System.out.println(x.b + " raised to the " + x.e + " power is " + x.get_pwr());
+    System.out.println(y.b + " raised to the " + y.e + " power is " + y.get_pwr());
+    System.out.println(z.b + " raised to the " + z.e + " power is " + z.get_pwr());
 
   }
 }
